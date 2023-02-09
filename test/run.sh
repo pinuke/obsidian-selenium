902f1db70997b3c4712a7b7008f272137448ffe0
smartguy1196 <smartguy1196@gmail.com>
2023-02-08 17:15:47 -0700

    test(test-init): :test_tube: implement xdg-open in unit test
    - add code to automatically open obsidian at the test path
    - typo fix in run.sh
    - fix .gitignore

--- test/run.sh
+++ test/run.sh
@@ -2,4 +2,5 @@ $branch = [[ $1 ]] && $1 || "main"
 mkdir "$branch-$(date +"%m.%d.%y-%H:%M:%S")"
 cd "$branch-$(date +"%m.%d.%y-%H:%M:%S")"
 cp ../../.obsidian .obsidian
-git clone -b $1 https://github.com/smartguy1196/obsidian-selenium .obsidian/pluginss
+git clone -b $1 https://github.com/smartguy1196/obsidian-selenium .obsidian/plugins
+xdg-open "obsidian://open?path=$(pwd)" </dev/null &>/dev/null &
