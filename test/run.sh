4f5fff02f9076e8e91aa21389d70255fb729d1a8
smartguy1196 <smartguy1196@gmail.com>
2023-02-08 17:23:48 -0700

    test(test-init): :sparkles: reimplement xdg-open to use a test file
    - change xdg-open argument
      - append `$testfile` to it
      - default: "README.md"

--- test/run.sh
+++ test/run.sh
@@ -1,6 +1,9 @@
 $branch = [[ $1 ]] && $1 || "main"
 mkdir "$branch-$(date +"%m.%d.%y-%H:%M:%S")"
+
+$testfile = [[ $2 ]] && $2 || "README.md"
+
 cd "$branch-$(date +"%m.%d.%y-%H:%M:%S")"
 cp ../../.obsidian .obsidian
 git clone -b $1 https://github.com/smartguy1196/obsidian-selenium .obsidian/plugins
-xdg-open "obsidian://open?path=$(pwd)" </dev/null &>/dev/null &
+xdg-open "obsidian://open?path=$(pwd)/$testfile" </dev/null &>/dev/null &
