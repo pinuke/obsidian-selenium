f9b77e0ac63586532ca1b5b301ab7b57e1523f17
smartguy1196 <smartguy1196@gmail.com>
2023-02-11 13:48:40 -0700

    fix(test-init): :fire: patch obsidian-uri handling - fix obsidian-uri handling of unrecognized vaults via:   - add vault entry to obsidian.json in obsidian's appdata   - add vault window settings record to `[vault_id]`.json
    - see https://forum.obsidian.md/t/using-the-obsidian-appdata-folder-for-unit-testing/54241

--- README.md
+++ README.md
@@ -16,6 +16,8 @@ However, the vaults (or copies of this repo) loaded into the `test/` directory u
 The directory where the source .md files for the Github Pages instance of this repo live. These docs are the user-facing docs.
 - see [Plugin User Docs](#plugin-user-docs)
 
+If you would like to see how I wrote the Plugin User Docs, see the [Just the Docs documentation](https://just-the-docs.github.io/just-the-docs/) and [my custom implementation of the gh-pages action](https://github.com/smartguy1196/obsidian-selenium/blob/main/.github/workflows/pages.yml). I'm happy to share how I built it.
+
 ### `test/` Directory:
 - see [Unit Testing](#unit-testing)
 - removed from Unit Tests, to prevent circular testing
@@ -28,13 +30,20 @@ Included with this repository is a `test/run.sh` bash script for automatically s
 
 ### `run.sh` Usage:
 
+This script automatically:
+- clones this repository into the test directory under the name `$branchname-$timestamp`. 
+- removes the test folder from the clone (prevents circular-testing lol). 
+- opens the vault using [the method described on one of my Obsidian Forum Thread](https://forum.obsidian.md/t/using-the-obsidian-appdata-folder-for-unit-testing/54241)
+
+#### Syntax:
+
 ```bash
 cd test
 ./run.sh $brachname $path
 ```
 
-#### `$branchname`:
+##### `$branchname`:
 - name of the branch that you would like to test. Default: `"main"`
 
-#### `$path`:
+##### `$path`:
 - relative path from the vault root to the testfile that you would like to have open when obsidian launches. Default: `"README.md"`
--- test/run.sh
+++ test/run.sh
@@ -1,12 +1,63 @@
-$branch = [[ $1 ]] && $1 || "main"
-mkdir "$branch-$(date +"%m.%d.%y-%H:%M:%S")"
+[[ $1 -ne "" ]] && branch=$1 || branch="main"
+unit_pathleaf="$branch-$(date +"%m.%d.%y-%H:%M:%S")"
+mkdir $unit_pathleaf
 
-$testfile = [[ $2 ]] && $2 || "README.md"
+[[ $2 -ne "" ]] && testfile=$2 || testfile="README.md"
 
-cd "$branch-$(date +"%m.%d.%y-%H:%M:%S")"
-cp ../../.obsidian .obsidian
-git clone -b $1 "$(git config --get remote.origin.url)" .
+cd "$unit_pathleaf"
+git clone "$(git config --get remote.origin.url)" .
+git switch $branch 2>/dev/null || git switch -c $branch;
 rm -r test
 # rm -r docs
-cp . .obsidian/plugins
+mkdir .obsidian/plugins
+cd .obsidian/plugins
+
+extglob_status=$(echo "$(shopt extglob)" | grep -o ..$)
+extglob_flag=false
+if [[ "$extglob_status" != "on" ]]; then
+    extglob_flag=true
+    shopt -s extglob
+fi
+
+cp -r ../../!(".obsidian") .
+
+if [[ $extglob_flag ]]; then
+    shopt -u extglob
+fi
+
+cd ../..
+
+# add vault to obsidian's internal vault tracker:
+
+temp_files="$(echo ~)/.config/obsidian"
+test -f "$temp_files/obsidian.json" || echo "{}" > "$temp_files/obsidian.json"
+if [[ "$(cat "$temp_files/obsidian.json")" == "" ]]; then
+    echo "{}" > "$temp_files/obsidian.json"
+fi
+
+node -e "$( cat << EOF
+    let { vaults } = require( "$temp_files/obsidian.json" )
+    if( !vaults ){
+        vaults = {}
+    }
+    let vault = vaults[ "$unit_pathleaf" ] = {
+        "path" : "$(pwd)",
+        "ts" : $(date +%s%3N)
+    }
+    json = JSON.stringify({ "vaults": vaults })
+    fs.writeFileSync( "$temp_files/obsidian.json", json, 'utf8' )
+    let vault_cache = {
+        "x": 0,
+        "y": 0,
+        "width" : 600,
+        "height" : 800,
+        "isMaximized" : true,
+        "devTools" : true,
+        "zoom" : 0
+    }
+    json = JSON.stringify(vault_cache)
+    fs.writeFileSync( "$temp_files/$unit_pathleaf.json", json, 'utf8' )
+EOF
+)"
+
 xdg-open "obsidian://open?path=$(pwd)/$testfile" </dev/null &>/dev/null &
