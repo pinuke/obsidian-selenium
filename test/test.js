1dfee62f0aa5193852291222e52c10d1ea32a65b
smartguy1196 <smartguy1196@gmail.com>
2023-02-13 02:44:11 -0700

    feat(test-init): :art: add pretty-printed logs
    - also add leveldb for later use (will be used to auto-enable plugins)

--- test/test.js
+++ test/test.js
@@ -1,226 +1,333 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const path = require( "path" )
-const fs = require( "fs-extra" ) // note this script adds the fs.cd() method!
-const open = require( "open" )
-const moment = require( "moment" )
-const globby = require( "globby" )
+(async function(){
 
-console.group( "SETTING UP ENVIRONMENT:" )
+    const path   = await import( "path" )
+    const fs     = await import( "fs-extra" )
+    const open   = await import( "open" )
+    const moment = await import( "moment" )
+    const globby = await import( "globby" )
+    const chalk  = await import( "chalk" )
+    const level  = await import( "level" )
 
-{ // globals factory
+    { // setup logging
 
-    console.group( "SETTING UP GLOBALS:" )
+        const log = {}
+        const levels = {
+
+            "help"  : "bgMagenta", //display help text
+
+            "trace" : "bgCyan",   //report location
+            "debug" : "bgBlue",   //report state of variables
+            "info"  : "bgWhite",  //report info for the operator
+            "warn"  : "bgYellow", //can the subprocess continue with the unwanted state?
+            "error" : "bgRed",    //can the app continue with the unwanted state?
+            "fatal" : "bgRed",    //crash the app
+            
+        }
+
+        const timestamp = () => chalk.dim(`[${moment().format( "HH:mm:ss" )}]`)
+
+        log.g = ( level, label )=>{
+
+            if( !label )
+                label = level.toUpperCase()
+
+            process.stdout.write( timestamp() )
+            console.group( chalk[ levels[ level ] ].bold( label ) )
+
+        }
+
+        log.g.end = ()=>{
+            console.groupEnd()
+            console.log()
+        }
+
+        log.trace = ( message="Trace Called", label="TRACE" ) => {
+
+            log.g( "trace", label )
+
+            let stack = new Error()
+                .split("\n").slice(1).join("\n") //remove the first line
+                .trim()                          //trim off any leading or trailing whitespace
+                .replaceAll(/^\s+/gm,'')         //trim off leading whitespace for everyline
+            console.log( `${ message }\n\n${ stack }` )
+
+            log.g.end()
+
+        }
+
+        log.debug = ( object, label="DEBUG" ) => {
+
+            log.g( "debug", label )
+
+            console.log( object )
+
+            log.g.end()
+
+        }
+
+        log.info = ( message, label=" INFO" ) => {
+            process.stdout.write( timestamp() )
+            console.log( `${ chalk[ levels[ "info" ] ]( label ) }: ${ message }` )
+        }
+        
+        log.warn = ( message, label=" WARN" ) => {
+            process.stdout.write( timestamp() )
+            console.log( `${ chalk[ levels[ "info" ] ]( label ) }: ${ chalk.underline( message ) }` )
+        }
+        
+        log.log = ( message ) => {
+            process.stdout.write( timestamp() + ": " )
+            console.log( `${ chalk.underline( message ) }` )
+        }
+
+        log.error = ( message, label="ERROR" ) => {
+            process.stdout.write( chalk.red(`[${moment().format( "HH:mm:ss" )}]`) )
+            console.log( `${ chalk[ levels[ "error" ] ]( label ) }: ${ message }` )
+            log.trace( "Error Occurred" )
+        }
+
+        log.fatal = ( message, label="FATAL" ) => {
+
+            log.error( message, label )
+            process.exit(1)
+
+        }
+
+        global.log = log
+
+    } // finish setting up logging
+
+    const log = global.log
+
+    log.g( "info", "PERFORMING INITIALIZATION:" )
+
+    { // globals factory
+
+        log.g( "info", "LOGGING SETUP. SETTING UP GLOBALS" )
+
+        log.log( "Adding .escape() method to RegExp..." )
+        { // [[return:string] function] RegExp.escape(): escapes all special regex characters in an input string
     
-    console.log( "Adding .escape() method to RegExp..." )
-    { // [[return:string] function] RegExp.escape(): escapes all special regex characters in an input string
-
-        RegExp.escape = pattern => {
-            return pattern.replace(/[/\-\\^$*+?.()|[\]{}]/g, '\\$&');
-        }
-
-    }
-
-    console.log( "Parsing Node.js's argv to something that makes sense..." )
-    { // [array] global.args: parsed arguments for script
-
-        const args = process.argv.slice()
-
-        if( args[0] == "node" )
-            args.shift()
-
-        while( -1 < args.findIndex( argument => {
-
-            return RegExp( `${
-                RegExp.escape( path.basename(__filename) )
-            }$`).test( argument )
-
-        })){
-            args.shift()
-        }
-
-        global.args = args
-
-    }
-
-    console.log( "Wrapping execSync() into global.shell()..." )
-    { // [function] global.shell(): wrapper function for execSync
-
-        const execSync = require( "child_process" ).execSync
+            RegExp.escape = pattern => {
+                return pattern.replace(/[/\-\\^$*+?.()|[\]{}]/g, '\\$&');
+            }
     
-        global.shell = command =>{
-            return execSync( command, { "timeout" : 30000, "stdio" : 'pipe'} ).toString().trim()
         }
+
+        log.log( "Parsing Node.js's argv to something that makes sense..." )
+        { // [array] global.args: parsed arguments for script
+    
+            const args = process.argv.slice()
+    
+            if( args[0] == "node" )
+                args.shift()
+    
+            while( -1 < args.findIndex( argument => {
+    
+                return RegExp( `${
+                    RegExp.escape( path.basename(__filename) )
+                }$`).test( argument )
+    
+            })){
+                args.shift()
+            }
+    
+            global.args = args
+    
+        }
+
+        log.log( "Wrapping execSync() into global.shell()..." )
+        { // [function] global.shell(): wrapper function for execSync
+    
+            const execSync = require( "child_process" ).execSync
+        
+            global.shell = command =>{
+                return execSync( command, { "timeout" : 30000, "stdio" : 'pipe'} ).toString().trim()
+            }
+    
+        }
+
+        log.log( "Wrapping write/readFileSync() into global.save/loadfile() respectively..." )
+        { // [function] global.savefile() and global.loadfile(): wrapper functions for writeFileSync and readFileSync
+    
+            global.loadfile = path => {
+                return fs.readFileSync( path, { 'encoding' : 'utf8' }).toString().trim()
+            }
+    
+            global.savefile = ( path, data, flag="w" ) =>{
+                return fs.writeFileSync( path, data, { 'encoding' : 'utf8', 'flag' : flag })
+            }
+    
+        }
+
+        log.log( "Creating alias fs.cd() (fs-extra) for process.chdir()..." )
+        { // [function] fs.cd(): wrapper function for process.chdir()
+    
+            fs.cd = process.chdir
+    
+        }
+
+        log.g.end()
 
     }
 
-    console.log( "Wrapping write/readFileSync() into global.save/loadfile() respectively..." )
-    { // [function] global.savefile() and global.loadfile(): wrapper functions for writeFileSync and readFileSync
+    log.g( "info", "SETTING UP ENVIRONMENT CONSTANTS:" )
 
-        global.loadfile = path => {
-            return fs.readFileSync( path, { 'encoding' : 'utf8' }).toString().trim()
-        }
 
-        global.savefile = ( path, data, flag="w" ) =>{
-            return fs.writeFileSync( path, data, { 'encoding' : 'utf8', 'flag' : flag })
-        }
 
+    const ENV = {
+        "BRANCH" : {
+            "DEFAULT"  : "main",
+            "DETECTED" : global.shell( "git rev-parse --abbrev-ref HEAD" ),
+            "ARGUMENT" : global.args[1]
+        },
+        "TESTFILE" : {
+            "DEFAULT"  : "README.md",
+            "ARGUMENT" : global.args[0]
+        },
+        "APPDATA" : {
+            "OBSIDIAN" : `${
+                process.env.APPDATA || 
+                    ( process.platform == 'darwin' ?
+                        process.env.HOME + '/Library/Application Support' :
+                        process.env.HOME + "/.config" )
+            }/obsidian`
+        },
+        "GIT" : {
+            "ROOT" : global.shell( "git rev-parse --show-toplevel" )
+        },
+        "TIMESTAMP" : moment()
     }
 
-    console.log( "Creating alias fs.cd() (fs-extra) for process.chdir()..." )
-    { // [function] fs.cd(): wrapper function for process.chdir()
+    ENV.BRANCH   .SELECTED = ENV.BRANCH   .ARGUMENT || ENV.BRANCH.DETECTED || ENV.BRANCH.DEFAULT
+    ENV.TESTFILE .SELECTED = ENV.TESTFILE .ARGUMENT || ENV.TESTFILE.DEFAULT
+    
+    ENV.GIT.NAME = path.basename( ENV.GIT.ROOT ) // this is the plugin's name
+    
+    // identify test by branch name and timestamp
+    // - this is also the name of the unit test folder
+    
+    ENV.ID = `${ ENV.BRANCH.SELECTED }-${ ENV.TIMESTAMP.format('mm.dd.yyyy-HH.MM.SS') }`
+    
+    log.debug( ENV, "ENVIRONMENT CONSTANTS:" )
 
-        fs.cd = process.chdir
+    log.g.end()
+    log.g.end()
 
+    log.g( "info", "BUILD TEST:" )
+
+    // ensure test directory and cd into it
+    fs.ensureDirSync( `${ ENV.GIT.ROOT }/test/${ ENV.ID }` );
+    fs.cd(            `${ ENV.GIT.ROOT }/test/${ ENV.ID }` );
+
+    // the root of this repository is a vault
+    // setup test using the above vault on the (argument-provided) branch
+    // - remove the test directory (prevents circular testing)
+    // - optionally remove the docs directory
+
+    ENV.OUTPUT = process.cwd()
+
+    log.warn( `cloning ${ ENV.GIT.ROOT } to...`, "GIT CLONE:")
+    console.log( ENV.OUTPUT )
+
+    global.shell( `git clone "${ ENV.GIT.ROOT }" .` )
+    global.shell( `git switch "${ ENV.BRANCH.SELECTED }" 2>/dev/null || global switch -c "${ ENV.BRANCH.SELECTED }"` )
+
+    log.log( "cleaning up clone..." )
+
+    fs.removeSync( 'test' )
+    // fs.removeSync( 'docs' )
+
+    // setup the plugin via copy (copy the modified root directory to the core of the plugin)
+    log.g( "warn", "PREPARING PLUGIN:" )
+
+    log.log( "copying files..." )
+    fs.ensureDirSync( `.obsidian/plugins/${ ENV.GIT.NAME }` )
+
+    // copy the entire root directory except the test, docs (optional - see above), and .obsidian folders to the plugins directory
+
+    globby.sync( "./!(.obsidian)" ).forEach( file => {
+
+        fs.copySync( file, `.obsidian/plugins/${ ENV.GIT.NAME }/${ file }` )
+
+    })
+
+    log.g.end()
+    log.g( "warn", "BUILDING PLUGIN:" )
+
+    // build the plugin
+
+    fs.cd( `.obsidian/plugins/${ ENV.GIT.NAME }` )
+    log.log( "installing dependencies..." )
+    global.shell( 'npm install --no-bin-links' )
+    log.log( "building..." )
+    global.shell( 'node esbuild.config.mjs test' )
+    fs.cd( '../../..' )    
+
+    log.g.end()
+    log.g.end()
+
+    log.g( "info", "SETTING UP VAULT:" )
+
+    log.log( "preparing vault tracker..." )
+    // the rest of the code (except the last line) adds the new vault to obsidian's internal vault tracker
+    
+    // write '{}' to obsidian's vault tracker if it doesn't exist or if it is empty (== '')
+    try{ global.savefile( `${ ENV.APPDATA.OBSIDIAN }/obsidian.json`, '{}', 'wx' ) } catch { /* Errors are expected */ }
+    if( global.loadfile(  `${ ENV.APPDATA.OBSIDIAN }/obsidian.json` ) == '' )
+        global.savefile(  `${ ENV.APPDATA.OBSIDIAN }/obsidian.json`, '{}' )
+
+    log.log( "updating vault tracker..." )
+    // get vaults list (hash table) from obsidian's vault tracker
+    
+    let { vaults } = require( `${ ENV.APPDATA.OBSIDIAN }/obsidian.json` )
+    
+    // if the vaults list is empty/null, create it
+    
+    if( !vaults )
+        vaults = {}
+    
+    // add the vault entry
+    // - obsidian URI starts by checking the vault tracker for this:
+    
+    vaults[ ENV.ID ] = {
+        "path" : process.cwd(),
+        "ts" : ENV.TIMESTAMP.toDate().getTime()
     }
+    
+    global.savefile(
+        `${ ENV.APPDATA.OBSIDIAN }/obsidian.json`,
+        JSON.stringify({ "vaults": vaults }))
 
-    console.groupEnd() // end of SETTING UP GLOBALS
+    log.log( "adding vault cache..." )
+    // create a vault cache
+    // - this is the second thing that the obsidian URI looks for:
+    
+    global.savefile(
+        `${ ENV.APPDATA.OBSIDIAN }/${ ENV.ID }.json`,
+        JSON.stringify({
+            "x": 0,
+            "y": 0,
+            "width" : 600,
+            "height" : 800,
+            "isMaximized" : true,
+            "devTools" : true,
+            "zoom" : 0
+        }))
+    
+    log.g.end()
 
-}
+    log.warn( "launching...", "GETTING READY!" )
+    open( `obsidian://open?path=${ process.cwd() }/${ ENV.TESTFILE.SELECTED }` )
+    log.log( "test launched!" )
+    console.log()
+    log.g( "help", "POSTINSTALL:" )
+    console.group( "run this to cd into the test repository:" )
+    console.log( `cd "${ENV.OUTPUT}"`)
+    console.groupEnd()
+    console.group( "run this to cd into the test plugin folder" )
+    console.log( `cd "${ENV.OUTPUT}/.obsidian/plugins/${ ENV.GIT.NAME }`)
+    log.g.end()
 
-console.group( "SETTING UP 'ENV' OBJECT:" )
-
-const ENV = {
-    "BRANCH" : {
-        "DEFAULT"  : "main",
-        "DETECTED" : global.shell( "git rev-parse --abbrev-ref HEAD" ),
-        "ARGUMENT" : global.args[1]
-    },
-    "TESTFILE" : {
-        "DEFAULT"  : "README.md",
-        "ARGUMENT" : global.args[0]
-    },
-    "APPDATA" : {
-        "OBSIDIAN" : `${
-            process.env.APPDATA || 
-                ( process.platform == 'darwin' ?
-                    process.env.HOME + '/Library/Application Support' :
-                    process.env.HOME + "/.config" )
-        }/obsidian`
-    },
-    "GIT" : {
-        "ROOT" : global.shell( "git rev-parse --show-toplevel" )
-    },
-    "TIMESTAMP" : moment()
-}
-
-ENV.BRANCH   .SELECTED = ENV.BRANCH   .ARGUMENT || ENV.BRANCH.DETECTED || ENV.BRANCH.DEFAULT
-ENV.TESTFILE .SELECTED = ENV.TESTFILE .ARGUMENT || ENV.TESTFILE.DEFAULT
-
-ENV.GIT.NAME = path.basename( ENV.GIT.ROOT ) // this is the plugin's name
-
-// identify test by branch name and timestamp
-// - this is also the name of the unit test folder
-
-ENV.ID = `${ ENV.BRANCH.SELECTED }-${ ENV.TIMESTAMP.format('mm.dd.yyyy-HH.MM.SS') }`
-
-console.log( ENV )
-console.groupEnd() // end of SETTING UP 'ENV' OBJECT
-console.groupEnd() // end of SETTING UP ENVIRONMENT
-
-console.group( "BUILD TEST:" )
-
-// ensure test directory and cd into it
-fs.ensureDirSync( `${ ENV.GIT.ROOT }/test/${ ENV.ID }` );
-fs.cd(            `${ ENV.GIT.ROOT }/test/${ ENV.ID }` );
-
-// the root of this repository is a vault
-// setup test using the above vault on the (argument-provided) branch
-// - remove the test directory (prevents circular testing)
-// - optionally remove the docs directory
-
-ENV.OUTPUT = process.cwd()
-console.log( `cloning ${ ENV.GIT.ROOT } to ${ ENV.OUTPUT } ...`)
-
-global.shell( `git clone "${ ENV.GIT.ROOT }" .` )
-global.shell( `git switch "${ ENV.BRANCH.SELECTED }" 2>/dev/null || global switch -c "${ ENV.BRANCH.SELECTED }"` )
-
-console.log( "cleaning up clone..." )
-
-fs.removeSync( 'test' )
-// fs.removeSync( 'docs' )
-
-// setup the plugin via copy (copy the modified root directory to the core of the plugin)
-
-console.log( "preparing plugin..." )
-
-fs.ensureDirSync( `.obsidian/plugins/${ ENV.GIT.NAME }` )
-
-// copy the entire root directory except the test, docs (optional - see above), and .obsidian folders to the plugins directory
-
-globby.sync( "./!(.obsidian)" ).forEach( file => {
-
-    fs.copySync( file, `.obsidian/plugins/${ ENV.GIT.NAME }/${ file }` )
-
-})
-
-console.log( "building plugin..." )
-// build the plugin
-
-fs.cd( `.obsidian/plugins/${ ENV.GIT.NAME }` )
-console.log( "installing dependencies..." )
-global.shell( 'npm install --no-bin-links' )
-console.log( "building..." )
-global.shell( 'node esbuild.config.mjs test' )
-fs.cd( '../../..' )
-
-console.groupEnd() // end of BUILD TEST
-console.group( "SETTING UP VAULT:" )
-
-console.log( "preparing vault tracker..." )
-// the rest of the code (except the last line) adds the new vault to obsidian's internal vault tracker
-
-// write '{}' to obsidian's vault tracker if it doesn't exist or if it is empty (== '')
-try{ global.savefile( `${ ENV.APPDATA.OBSIDIAN }/obsidian.json`, '{}', 'wx' ) } catch { /* Errors are expected */ }
-if( global.loadfile(  `${ ENV.APPDATA.OBSIDIAN }/obsidian.json` ) == '' )
-    global.savefile(  `${ ENV.APPDATA.OBSIDIAN }/obsidian.json`, '{}' )
-
-console.log( "updating vault tracker..." )
-// get vaults list (hash table) from obsidian's vault tracker
-
-let { vaults } = require( `${ ENV.APPDATA.OBSIDIAN }/obsidian.json` )
-
-// if the vaults list is empty/null, create it
-
-if( !vaults )
-    vaults = {}
-
-// add the vault entry
-// - obsidian URI starts by checking the vault tracker for this:
-
-vaults[ ENV.ID ] = {
-    "path" : process.cwd(),
-    "ts" : ENV.TIMESTAMP.toDate().getTime()
-}
-
-global.savefile(
-    `${ ENV.APPDATA.OBSIDIAN }/obsidian.json`,
-    JSON.stringify({ "vaults": vaults }))
-
-console.log( "adding vault cache..." )
-// create a vault cache
-// - this is the second thing that the obsidian URI looks for:
-
-global.savefile(
-    `${ ENV.APPDATA.OBSIDIAN }/${ ENV.ID }.json`,
-    JSON.stringify({
-        "x": 0,
-        "y": 0,
-        "width" : 600,
-        "height" : 800,
-        "isMaximized" : true,
-        "devTools" : true,
-        "zoom" : 0
-    }))
-
-console.groupEnd() // end of SETTING UP VAULT
-// Open the new vault using the URI
-
-console.log( "launching..." )
-open( `obsidian://open?path=${ process.cwd() }/${ ENV.TESTFILE.SELECTED }` )
-console.log( "test launched!" )
-console.log()
-console.group( "run this to cd into the test repository:" )
-console.log( `cd "${ENV.OUTPUT}"`)
-console.groupEnd()
-console.group( "run this to cd into the test plugin folder" )
-console.log( `cd "${ENV.OUTPUT}/.obsidian/plugins/${ ENV.GIT.NAME }`)
+})()
